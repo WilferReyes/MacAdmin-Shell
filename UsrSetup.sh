46862cd3b980818916283e5f66290eb38a26e383
Wilfer Reyes <50413005+WilferReyes@users.noreply.github.com>
2019-05-15 11:08:25 -0400

    Update UsrSetup.sh

--- UsrSetup.sh
+++ UsrSetup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo dscl . -create /Users/#Account 
-sudo dscl . -create /Users/#Account UserShell /bin/bash
-sudo dscl . -create /Users/#Account RealName "" #Account
-sudo dscl . -create /Users/#Account UniqueID ""
-sudo dscl . -create /Users/#Account PrimaryGroupID 80
-sudo dscl . -create /Users/#Account NFSHomeDirectory /Users/#Account
-sudo dscl . -passwd /Users/#Account #Password
-sudo dscl . -append /Groups/() GroupMembership Generic
+sudo dscl . -create /Users/<AccountName>
+sudo dscl . -create /Users/<AccountName> UserShell /bin/bash
+sudo dscl . -create /Users/<AccountName> RealName " <Name> " <AccountName>
+sudo dscl . -create /Users/<AccountName> UniqueID " <Number> "
+sudo dscl . -create /Users/<AccountName> PrimaryGroupID 80
+sudo dscl . -create /Users/<AccountName> NFSHomeDirectory /Users/<AccountName>
+sudo dscl . -passwd /Users/<AccountName> <Password>
+sudo dscl . -append /Groups/<GroupName> GroupMembership <Memebership>
 
 exit 0
